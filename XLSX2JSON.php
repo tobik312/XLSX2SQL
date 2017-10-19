394ef0c7c1eaf58f968d7848058cb4501d8a7cb5
tobik312 <tobik-10@tlen.pl>
2017-10-19 15:23:43 +0200

    update

--- XLSX2JSON.php
+++ XLSX2JSON.php
@@ -1,10 +1,18 @@
 <?php
 class XLSX2JSON{
 
+    private header = array();
+
     function __construct($elem){
         if(get_class($elem)!="XLSXElement") return;
+        foreach($elem->getSheetList() as $id){
+            $cols = $elem->getSheetColumns($id);
+            $header = $elem->getSheetRow($id,0);
 
+        }
     }
+
+
 
 }
 ?>

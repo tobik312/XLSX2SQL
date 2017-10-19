d92b361d6d2f73776ec68f0f05063df8ae538f78
tobik312 <tobik-10@tlen.pl>
2017-10-19 18:35:47 +0200

    update

--- XLSX2JSON.php
+++ XLSX2JSON.php
@@ -1,23 +1,45 @@
 <?php
 class XLSX2JSON{
 
-    private headers = array();
+    private $elem;
+    private $headers = array();
+    private $cols = array();
+    private $jsonArray;
 
     function __construct($elem){
         if(get_class($elem)!="XLSXElement") return;
-        foreach($elem->getSheetList() as $id){
-            $cols = $elem->getSheetColumns($id);
-            $header[] = $elem->getSheetRow($id,0);
+        $this->elem = $elem;
+        foreach($this->elem->getSheetList() as $id){
+            $this->cols[$id][] = $this->elem->getSheetColumns($id);
+            $this->header[$id] = $this->elem->getSheetRow($id,0);
         }
+        $this->compile();
+    }
+
+    private function compile(){
+        $tmp = array();
+        foreach($this->header as $sheetKey=>)
     }
 
     //key,key,key
     function setSheet(...$keys){
-
+        $this->cols = array();
+        foreach($keys as $key){
+            $this->cols[] = $this->elem->getSheetColumns($key);
+        }
+        $this->compile();
     }
 
     //array(sheetKey=>array(headers))
     function setHeaders($headers){
+        $this->header = array();
+        foreach($headers as $key=>$headers){
+            $this->header[$key] = $headers;
+        }
+        $this->compile();
+    }
+
+    function getJSONString(){
 
     }
 
